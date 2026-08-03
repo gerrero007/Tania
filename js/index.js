c5ade027177fdbda309cfbcb5ccf53e93b84e1c9
Nahuel <gerreropiconi@gmail.com>
2026-08-03 13:29:13 +0200

    feat: archivos creados y index

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+
+    setInterval(() => {
+        img1.src = `../img/${Math.floor(Math.random() * (26 - 1 + 1)) + 1}.jpg`
+        img2.src = `../img/${Math.floor(Math.random() * (26 - 1 + 1)) + 1}.jpg`
+    }, 10000)
+})
