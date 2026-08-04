3b9c84a2a64768bbc1baae8a1d4eb9b20b2741a1
Nahuel <gerreropiconi@gmail.com>
2026-08-04 11:24:49 +0200

    feat: añadidos las dos primeras partes de viajes, el grupo y elprincipio

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const img2 = document.getElementById("img2");
 
     setInterval(() => {
-        img1.src = `/img/${Math.floor(Math.random() * (26 - 1 + 1)) + 1}.jpg`
-        img2.src = `/img/${Math.floor(Math.random() * (26 - 1 + 1)) + 1}.jpg`
+        img1.src = `/img/${Math.floor(Math.random() * (33 - 1 + 1)) + 1}.jpg`
+        img2.src = `/img/${Math.floor(Math.random() * (33 - 1 + 1)) + 1}.jpg`
     }, 10000)
 })
